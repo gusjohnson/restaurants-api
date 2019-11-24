2ddff34652fd8c8eb271d9214fd2025d767e84ef
gusjohnson <enjoisf@gmail.com>
2019-11-24 15:44:12 -0600

    cors fix

--- server.js
+++ server.js
@@ -3,9 +3,11 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
+const cors = require('cors')
 
 const app = express()
 app.use(bodyParser.json())
+app.use(cors())
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/test', {
   useNewUrlParser: true,
